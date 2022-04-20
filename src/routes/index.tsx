6426b8c90a55bbdc9af199dda53d0b8a572388f7
AutumnDeSea <1337026037@qq.com>
2022-04-20 18:44:53 +0800

    fix: tailwind默认主题设置

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import Layout from '@/layout';
 import HomePage from '@/pages/HomePage';
 import AboutUsPage from '@/pages/AboutUs';
 import NotFoundPage from '@/pages/NotFound';
-import TopArticlePage from '@/pages/TopArticle';
+import TopArticlePage from '@/pages/topArticle';
 const routes: RouteObject[] = [
   {
     path: '/', 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ['./src/**/*.tsx', './src/*.tsx'],
   theme: {
+    colors: {
+      "black": "rgba(25, 25, 25, 1)",
+      "slim-black": "rgb(41, 41, 41)",
+      "gray": "rgb(117, 117, 117)"
+    },
     extend: {},
   },
   variants: {},
