1de9499f459d36bdce5a8b3dbcf80d6ca1ce33ad
boyan <846184637@qq.com>
2022-04-22 00:38:43 +0800

    chore:  conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   content: ['./src/web/**/*.tsx', './src/web/*.tsx'],
   theme: {
-<<<<<<< HEAD
-=======
     colors: {
       "black": "rgba(25, 25, 25, 1)",
       "slim-black": "rgb(41, 41, 41)",
@@ -10,7 +8,6 @@ module.exports = {
       "white": "rgb(255, 255, 255,1)",
       "yellow": "#FFC017"
     },
->>>>>>> f0f0f52 (feature: header-component)
     extend: {},
   },
   variants: {},
