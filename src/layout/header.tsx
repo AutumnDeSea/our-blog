c5e0bb610136a47529148827d0ebd83fbcb4a448
boyan <846184637@qq.com>
2022-04-21 23:57:01 +0800

    feature: header-component

---
+++ src/layout/header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <div className="bg-yellow fixed w-full top-0 left-0">
+      <header className="flex justify-between p-5 max-w-7xl mx-auto">
+        <div className="flex items-center space-x-5">
+          <Link to="/home">
+            <img
+              className="w-44 object-contain cursor-pointer"
+              src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="flex items-center space-x-5 text-black">
+          <h3>Our Story </h3>
+          <h3> Memory </h3>
+          <h3>writer</h3>
+          <h3>sign in </h3>
+          <h3 className="text-white bg-black rounded-full px-4 py-2">
+            Get Started
+          </h3>
+        </div>
+      </header>
+    </div>
+  );
+};
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ['./src/web/**/*.tsx', './src/web/*.tsx'],
   theme: {
+<<<<<<< HEAD
+=======
+    colors: {
+      "black": "rgba(25, 25, 25, 1)",
+      "slim-black": "rgb(41, 41, 41)",
+      "gray": "rgb(117, 117, 117)",
+      "white": "rgb(255, 255, 255,1)",
+      "yellow": "#FFC017"
+    },
+>>>>>>> f0f0f52 (feature: header-component)
     extend: {},
   },
   variants: {},
